276ca558e0bd4f16cf75651fbd2e7f171d4046df
Galen <1453811292@qq.com>
2025-03-21 15:10:15 +0800

    feat: coroutine

---
+++ process/coroutine.py
@@ -0,0 +1,32 @@
+# 协程 
+# 子程序调用总是一个入口，一次返回，调用顺序是明确的。而协程的调用和子程序不同。
+# 协程看上去也是子程序，但执行过程中，在子程序内部可中断，然后转而执行别的子程序，在适当的时候再返回来接着执行。
+# 协程的特点在于是一个线程执行，那和多线程比，协程有何优势？    
+# 最大的优势就是协程极高的执行效率。因为子程序切换不是线程切换，而是由程序自身控制，因此，没有线程切换的开销，和多线程比，线程数量越多，协程的性能优势就越明显。    
+
+
+# 消费者
+def consumer():
+    r = ''
+    while True:
+        n = yield r
+        if not n:
+            return
+        print('[CONSUMER] Consuming %s...' % n)
+        r = '200 OK'
+
+
+# 生产者
+def produce(c):
+    c.send(None)
+    n = 0
+    while n < 5:
+        n = n + 1
+        print('[PRODUCER] Producing %s...' % n)
+        r = c.send(n)
+        print('[PRODUCER] Consumer return: %s' % r)
+    c.close()
+
+
+c = consumer()
+produce(c)
